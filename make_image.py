fb888c120609c680e211bb4ff3b4859c8183934a
Adrianna Pińska <adrianna.pinska@gmail.com>
2019-11-13 11:46:04 -0700

    add feature to add nans in various patterns

--- make_image.py
+++ make_image.py
@@ -10,8 +10,11 @@ import sys
 import numpy as np
 from astropy.io import fits
 
+NAN_OPTIONS = ("pixel", "row", "column", "channel", "stokes", "image")
+
 def make_image(args):
     dims = tuple(args.dimensions)
+    N = len(dims)
 
     # create header
 
@@ -49,14 +52,63 @@ def make_image(args):
         strip_size = args.max_bytes // np.dtype(np.float32).itemsize
 
     total_size = np.product(dims)
-    rounded_size = strip_size * (total_size // strip_size)
-    remainder = total_size - rounded_size
+    remainder = total_size % strip_size
+    rounded_size = total_size - remainder
     contiguous_data = data.ravel()
+    
+    def nan_sample(start, end):
+        nan_sample_size = int(np.round((args.nan_density/100) * (end - start)))
+        return np.random.choice(range(start, end), nan_sample_size, False).astype(int)
 
-    for i in range(0, rounded_size, strip_size):
-        contiguous_data[i:i+strip_size] = np.random.default_rng().normal(size=strip_size).astype(np.float32)
+    if "image" in args.nans:
+        for i in range(0, rounded_size, strip_size):
+            contiguous_data[i:i+strip_size] = np.nan
+        contiguous_data[rounded_size:total_size] = np.nan
+    else:
+        for i in range(0, rounded_size, strip_size):
+            contiguous_data[i:i+strip_size] = np.random.default_rng().normal(size=strip_size).astype(np.float32)
+            if "pixel" in args.nans:
+                contiguous_data[nan_sample(i, i + strip_size)] = np.nan
 
-    contiguous_data[rounded_size:total_size] = np.random.default_rng().normal(size=remainder).astype(np.float32)
+        if remainder:
+            contiguous_data[rounded_size:total_size] = np.random.default_rng().normal(size=remainder).astype(np.float32)
+            if "pixel" in args.nans:
+                contiguous_data[nan_sample(rounded_size, total_size)] = np.nan
+    
+        width, height = dims[0:2]
+        channel_size = width * height
+        depth = dims[2] if N > 2 else 1
+        stokes = dims[3] if N > 3 else 1
+    
+        # add row, column, channel and stokes nans here in separate passes
+        # For now an implementation which ignores max_bytes
+        
+        shaped_data = data.reshape(stokes, depth, width, height)
+        
+        if "stokes" in args.nans:
+            nan_stokes = nan_sample(0, stokes)
+            for s in nan_stokes:
+                shaped_data[s] = np.nan
+        
+        if "channel" in args.nans:
+            nan_channels = nan_sample(0, depth * stokes)
+            for channel in nan_channels:
+                s, c = divmod(channel, channel_size)
+                shaped_data[s,c] = np.nan
+                
+        if "row" in args.nans:
+            nan_rows = nan_sample(0, height * depth * stokes)
+            for row in nan_rows:
+                s, row = divmod(row, channel_size)
+                c, y = divmod(row, height)
+                shaped_data[s,c,y] = np.nan
+                
+        if "column" in args.nans:
+            nan_columns = nan_sample(0, width * depth * stokes)
+            for column in nan_columns:
+                s, column = divmod(column, channel_size)
+                c, x = divmod(column, width)
+                shaped_data[s,c,:,x] = np.nan
 
     hdul.close()
 
@@ -64,6 +116,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate a synthetic FITS file.")
     parser.add_argument("dimensions", metavar="N", type=int, nargs="+", help="The dimensions of the file, in order (XY, XYZ or XYZW), separated by spaces.")
     parser.add_argument("-m", "--max-bytes", type=int, help="The maximum size of image data (in bytes) to create in memory at once. Default is the channel size.")
+    
+    parser.add_argument("-n", "--nans", nargs="+", help="Options for inserting random NaNs, which are cumulative, separated by spaces. Any combination of %s. By default no NaNs are inserted. 'image' overrides all other options and creates an image full of NaNs. Channel, row and column algorithm currently ignores the --max-bytes restriction." % ", ".join(repr(o) for o in NAN_OPTIONS), default=[])
+    parser.add_argument("-d", "--nan-density", type=float, help="The density of NaNs to insert, as a percentage. Default: 25. Ignored if -n/--nans is unset.", default=25.0)
+    
     parser.add_argument("-o", "--output", help="The output file name.")
 
     args = parser.parse_args()
